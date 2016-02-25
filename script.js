a3a356202d271bcd9633aad746e6476cfeeac3ac
nanselmo <nickianselmo@gmail.com>
2016-02-25 16:32:42 -0600

    full solution

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ $(document).ready(function(){
         $("#pig").animate({left: '2000px'});
     });
     //add a new event handler that causes the pig to slide up and down 3 times when the pig is clicked
-
-    //add a new pig that flies away once the first pig has exited the screen
+    $("#pig").click(function(){
+      for (i = 0; i < 3; i++) {
+        $("#pig").animate({top: '-=20px'}, "fast");
+        $("#pig").animate({top: '+=20px'}, "fast");
+      }
+    });
+    //STRETCH add a new pig that flies away once the first pig has exited the screen.
+    //You will have to put the animation of the second pig inside a callback function.
+    $("#pig2").click(function(){
+        $("#pig").animate({left: '2000px'}, "slow", function(){
+          $("#pig2").animate({left: '2000px'}, "slow");
+          });
+   });
 
 });
