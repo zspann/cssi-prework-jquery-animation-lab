2b30d4029a6481d82f3ef6f7a4c85f33f83737d6
Daniel Fenjves <dfenjves@users.noreply.github.com>
2016-02-28 13:52:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
     //add a third button that makes the pig exit to the right when the new button is clicked
 
     //add a new event handler that causes the pig to slide up and down 3 times when the pig is clicked
-    //use a loop and relative values (-= or +=)
+    //(hint: use a loop and relative values (-= or +=))
 
     //STRETCH add a new pig that flies away once the first pig has exited the screen.
-    //You will have to put the animation of the second pig inside a callback function.
+    //(hint: You will have to put the animation of the second pig inside a callback function.)
 
 });
